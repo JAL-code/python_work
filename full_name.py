c475457519a36afa185e20bb630c26d6870e452b
JAL-code <jleffek@live.com>
2021-03-18 12:25:15 -0400

    Revert "Same file in main branch except using format function (Python 3.5) and earlier."
    This reverts commit 3da9f49ee3a4d9575e3e88d35b52c69b36114f46.
    See branch fstring_prior_3_5.

--- full_name.py
+++ full_name.py
@@ -1,9 +1,7 @@
-#Python Crash Course, Eric Matthes, Chapter 2, 
-#format function with strings up to Python 3.5
-
+#Python Crash Course, Eric Matthes, Chapter 2, working with f-strings
 first_name = "ada"
 last_name = "lovelace"
-full_name = "{} {}".format(first_name, last_name)
-message = "Hello, {}!".format(full_name.title())
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
 print(message)
 
