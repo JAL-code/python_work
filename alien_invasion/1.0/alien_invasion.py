ba0f8aa77f42a43a97c611c896f9ce3afea8102a
JAL-code <jleffek@live.com>
2021-03-21 16:03:39 -0400

    Added ship parameter files.

--- alien_invasion/1.0/alien_invasion.py
+++ alien_invasion/1.0/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 import sys
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
   """Overall class to merge game assets and behavior."""
@@ -22,6 +23,8 @@ class AlienInvasion:
     self.screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion 1.0")
 
+    self.ship = Ship(self)
+
   def run_game(self):
     """Start the main loop for  the game."""
     while True:
@@ -32,6 +35,7 @@ class AlienInvasion:
 
       # Redraw the screen during each pass throught the loop
       self.screen.fill(self.settings.bg_color)
+      self.ship.blitme()
 
       # Make the most recently drawn screen visible.
       pygame.display.flip()
