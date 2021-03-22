91d913b63ef726219c2ad7b6c9412b4264a0e26c
JAL-code <jleffek@live.com>
2021-03-22 11:10:03 -0400

    Basics about setting up the loop added.

--- lists/magicians.py
+++ lists/magicians.py
@@ -1,3 +1,14 @@
-magicians = ['alice', 'david', 'carolina']
-for magicians in magicians:
-    print(magicians)
+# For every magician in the list of magicians, print the magicians name.
+# Singular name for action being done on each item.
+# Plural names for the list.
+magicians = ['alice', 'david', 'caroline']
+for magician in magicians:
+    print(magician)
+
+# Do more complex actions.
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+# Summarize the operation
+print(f"Thank you, everyone. That was a great show!")
