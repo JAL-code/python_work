a9c5b0d558a0f2acaa4b84f6840f68e18fb032d5
JAL-code <jleffek@live.com>
2021-03-22 22:36:59 -0400

    Add inequality and numerical comparisons.

---
+++ if_statement/toppings.py
@@ -0,0 +1,8 @@
+# Python Crash Course: Chapter 5, Eric Matthews, if Statements
+# Window 10
+
+#test for inequality or "not"
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
