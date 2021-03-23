fcd2fa3145fd90406e8e4c992680d9d766e9dccb
JAL-code <jleffek@live.com>
2021-03-23 13:49:05 -0400

    Basic input function for python
    parrot.py:  Basic input function for python 3.+.  Also logging actions for different programs. Request a check for program with Linux Mint system.
    parrot.cpython-38.pyc: Cpython version of program.

---
+++ user_input_while_loops/parrot.py
@@ -0,0 +1,15 @@
+# Python Crash Course: Chapter 5, Eric Matthews, Dictionaries
+# Windows 10
+
+# the input function recieves text as a string
+message = input("Tell me something, and I will repeat it "
+                "back to you: ")
+print(message)
+
+# This code runs from Visio Studio Code
+# This code does not run from Atom
+# This code runs from Python terminal:
+#  1. import os
+#  2. chd = os.chdir('C:/User/[username]/GitHub/python_work/user_input_while_loops')
+#  3. import parrot
+#  4. code runs in python terminal
