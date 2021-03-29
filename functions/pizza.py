c4a093d122b9ce83048a7e8d937ae611885fc0c1
JAL-code <jleffek@live.com>
2021-03-29 15:34:00 -0400

    For block added to print out ingredients one per line.

--- functions/pizza.py
+++ functions/pizza.py
@@ -3,7 +3,9 @@
 def make_pizza(*toppings):
     """Print the list of toppings that have been requested.
     *variable will create an empty tuple and pack the values inside."""
-    print(toppings)
+    print("\n Making a pizza with the following toppings: ")
+    for topping in toppings:
+        print(f"- {topping}")
 
 make_pizza('pepperoni')
 make_pizza('mushrooms', 'green peppers', 'extra cheese')
