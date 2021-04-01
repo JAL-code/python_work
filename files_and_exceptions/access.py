762ef54e4ec9684ff0cdb1ae0dd16db378f39b66
JAL-code <jleffek@live.com>
2021-04-01 10:27:08 -0400

    Setup file_reader.py to output a number
    Added pi_million_digits.txt to folder

--- files_and_exceptions/access.py
+++ files_and_exceptions/access.py
@@ -1,4 +1,6 @@
 
 def get_location_for_code():
-    access_area = "/home/joseph/python_work/files_and_exceptions/"
+    # :releative location
+    # access_area = 'files_and_exceptions/'
+    access_area = '/home/joseph/python_work/files_and_exceptions/'
     return access_area
--- files_and_exceptions/file_reader.py
+++ files_and_exceptions/file_reader.py
@@ -1,16 +1,39 @@
 # Python Crash Course: Chapter 10, Eric Matthews, Files and Exceptions
 
 from access import get_location_for_code
-# create a module called access.py that gives the location for the file_reader to get this macro to run.  Call the function: get_location_for_code()
+# create a module called access.py that gives the location
+# for the file_reader to get this macro to run.
+# Call the function: get_location_for_code()
 
 file_path = get_location_for_code()
+
 file_name = 'pi_digits.txt'
 with open(f"{file_path}{file_name}") as file_object:
-    contents = file_object.read()
-# this code relies on python to close() the file.  Hence close is not used in this example.
+    # load the file into a list
+    lines = file_object.readlines()
+# this code relies on python to close() the file.
+# Hence close is not used in this example.
+# print the lines from the list.
+for line in lines:
+    print(line.rstrip())
 
-# print whitespace (blank line)
-print(contents)
+# print the lines from the list joined.
+pi_string = ''
+pi_string2 = ''
+for line in lines:
+    pi_string += line.rstrip()
 
-# remove whitespace
-print(contents.rstrip())
+print(pi_string)
+print(f"{len(pi_string)} characters.")
+
+# print the lines from the list without the left spaces for indentation.
+
+for line in lines:
+    pi_string2 += line.strip()
+
+print(pi_string2)
+print(f"{len(pi_string2)} characters.")
+
+# This value is a string. Convert to float for numerical operations.
+print(type(pi_string2))
+print(f"A 3 ft circle has a circumference of {2*float(pi_string2)*3} ft.")
