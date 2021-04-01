46a5b6146840b3fd3f0ded18cd708a8bc372045c
JAL-code <jleffek@live.com>
2021-04-01 15:05:33 -0400

    Combined both remember_me-greet-user
    Combined remember_me.py and greet_user.py into one file.
    For the first time, it writes a new JSON file with username.
    For future logins, it just reads the file.

---
+++ files_and_exceptions/remember_me_combined.py
@@ -0,0 +1,17 @@
+# Python Crash Course: Chapter 10, Eric Matthews, Files and Exceptions
+import json
+
+# Load the username, if it has been stored previously.
+# Otherwise, prompt for the username and store it.
+
+filename = 'username.json'
+try:
+    with open(filename) as f:
+        username = json.load(f)
+except FileNotFoundError:
+    username = input("What is your username? ")
+    with open(filename, 'w') as f: 
+        json.dump(username, f)
+        print(f"We'll remember you when you come back online, {username}!")
+else:
+    print(f"Welcome back, {username}!")
