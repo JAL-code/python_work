06388be226510161a55221db6f170e831a285187
JAL-code <jleffek@live.com>
2021-04-01 13:34:08 -0400

    While loop with a try-except-else block.

--- files_and_exceptions/division_calculator.py
+++ files_and_exceptions/division_calculator.py
@@ -3,8 +3,22 @@
 # This code generates a ZeroDivisionError
 # print(5/0)
 # Use try-except Blocks to handle the exception.
+# Enter the error type and then the message for the error.
 
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("You can not divide by zero.")
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+# code block that calculates division with error exception.
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("\nFirst number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can not divide by zero.")
+    else:
+        print(answer)
