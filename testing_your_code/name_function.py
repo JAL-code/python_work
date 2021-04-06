c4fcfc8cac55e16379aac3ee2956264a04051921
JAL-code <jleffek@live.com>
2021-04-06 11:36:58 -0400

    Change the input, add middle name to mismatch the original test case.

--- testing_your_code/name_function.py
+++ testing_your_code/name_function.py
@@ -1,7 +1,6 @@
 # Python Crash Course: Chapter 11, Eric Matthews, Testing Your Code
 
-def get_formatted_name(first, last):
+def get_formatted_name(first, middle, last):
     '''Generate a neatly formatted full name.'''
-    full_name = f'{first} {last}'
+    full_name = f'{first} {middle} {last}'
     return full_name.title()
-
