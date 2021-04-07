9215ed5710dc57c989bc92ca2656abc3394953a4
JAL-code <jleffek@live.com>
2021-04-07 19:23:43 -0400

    Added version control and handle for Idle quit bug.
    Added self.version to control folder for version.
    Added pygame.quit() before sys.exit() to allow exit without requiring
    a forced quit.

--- alien_invasion/1.0/alien_invasion.py
+++ alien_invasion/1.0/alien_invasion.py
@@ -6,50 +6,51 @@ from settings import Settings
 from ship import Ship
 
 class AlienInvasion:
-  """Overall class to merge game assets and behavior."""
+    """Overall class to merge game assets and behavior."""
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        # Init an instance of the Settings
+        self.settings = Settings()
+        self.version = "1.0"
 
-  def __init__(self):
-    """Initialize the game, and create game resources."""
-    pygame.init()
-    # Init an instance of the Settings
-    self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
 
-    self.screen = pygame.display.set_mode(
-      (self.settings.screen_width, self.settings.screen_height))
+        # Set the background color
+        self.bg_color = (230, 230, 230)
 
-    # Set the background color
-    self.bg_color = (230, 230, 230)
+        self.screen = pygame.display.set_mode((1200, 800))
+        versiontitle = f"Alien Invasion {self.version}"
+        pygame.display.set_caption(versiontitle)
 
-    self.screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion 1.0")
+        self.ship = Ship(self)
 
-    self.ship = Ship(self)
+    def run_game(self):
+        """Start the main loop for  the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            self._check_events()
+            self._update_screen()
 
+    def _check_events(self):
+        """Helper method: Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit() # Added to avoid hang bug.
+                sys.exit()
 
+    def _update_screen(self):
+        """Helper method: Update images on the screen,
+        flip to the next screen. """
+        # Redraw the screen during each pass throught the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-  def run_game(self):
-    """Start the main loop for  the game."""
-    while True:
-      # Watch for keyboard and mouse events.
-      self._check_events()
-      self._update_screen()
-
-  def _check_events(self):
-    """Helper method: Respond to keypresses and mouse events."""
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        sys.exit()
-
-  def _update_screen(self)
-    """Helper method: Update images on the screen,flip to the next screen
-    # Redraw the screen during each pass throught the loop
-    self.screen.fill(self.settings.bg_color)
-    self.ship.blitme()
-
-      # Make the most recently drawn screen visible.
-      pygame.display.flip()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == '__main__':
-  # Make a game instance, and run the game.
-  ai = AlienInvasion()
-  ai.run_game()
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
