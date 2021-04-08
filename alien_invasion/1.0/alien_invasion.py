3dce7ecc8cee687205d6d9419738c77c28a46d3a
JAL-code <jleffek@live.com>
2021-04-07 23:01:03 -0400

    Added bullet sprits limit to 3 at a time.

--- alien_invasion/1.0/alien_invasion.py
+++ alien_invasion/1.0/alien_invasion.py
@@ -4,6 +4,7 @@ import sys
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to merge game assets and behavior."""
@@ -26,6 +27,7 @@ class AlienInvasion:
         pygame.display.set_caption(versiontitle)
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for  the game."""
@@ -33,7 +35,14 @@ class AlienInvasion:
             # Watch for keyboard and mouse events.
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
+
+            # Get rid of bullets that have disappeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
 
     def _check_events(self):
         """Helper method: Respond to keypresses and mouse events."""
@@ -66,6 +75,8 @@ class AlienInvasion:
             print(self.screen.get_rect().width) # 1920
             self.settings.screen_height = self.screen.get_rect().height
             print(self.screen.get_rect().height) # 1080
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Respond to key releases"""
@@ -76,12 +87,20 @@ class AlienInvasion:
             #Stop moving left 
             self.ship.moving_left = False        
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Helper method: Update images on the screen,
         flip to the next screen. """
         # Redraw the screen during each pass throught the loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
