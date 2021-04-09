16b1a52ce4315aa82e55d371af9d458b91f90868
JAL-code <jleffek@live.com>
2021-04-09 16:49:40 -0400

    Enable dynmaic increases in speed of ship, bullet and alien.

--- alien_invasion/1.0/settings.py
+++ alien_invasion/1.0/settings.py
@@ -18,10 +18,10 @@ class Settings:
 
         # Alien settings
         # Control the speed
-        self.alien_speed = 5.0  # was 1.0
+        self.alien_speed = 1.0
 
         # Bullet settings
-        self.bullet_speed = 3  # was 1.5
+        self.bullet_speed = 1.5  
         if self.bonus:
             self.bullet_width = 300
         else:
@@ -29,3 +29,24 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """ Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+        print(f"S: {self.ship_speed} B: {self.bullet_speed} A: {self.alien_speed}")
