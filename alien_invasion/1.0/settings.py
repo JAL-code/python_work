d16ce662d854ef65ce1a38df9d676379bdfd7052
JAL-code <jleffek@live.com>
2021-04-09 13:49:39 -0400

    Add bonus width for bullet to destory more aliens.
    Set bonus width to 300.  See self.bounus to True to activate bonus. Set to false for bonus timeout.

--- alien_invasion/1.0/settings.py
+++ alien_invasion/1.0/settings.py
@@ -7,6 +7,7 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        self.bonus = False
 
         # Ship settings
         self.ship_speed = 1.5
@@ -20,7 +21,10 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 1.0
-        self.bullet_width = 3
+        if self.bonus:
+            self.bullet_width = 300
+        else:
+            self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
