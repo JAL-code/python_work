4e7027c20a920ec5958c93091b18db5e766ff020
JAL-code <jleffek@live.com>
2021-04-11 20:15:23 -0400

    Long list of squares with graph range in list

--- data_analysis/scatter_squares.py
+++ data_analysis/scatter_squares.py
@@ -1,18 +1,22 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1000)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+# smaller size for more values
+ax.scatter(x_values, y_values, s=10) 
 
 # Set chart title and label axis.
-ax.set_title("Square Numbers", fontsize=14)
+ax.set_title("Long List of Square Numbers", fontsize=14)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of value", fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axis.
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
