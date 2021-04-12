a14153e62d041bb1eb025a40187d5bfdadb7859a
JAL-code <jleffek@live.com>
2021-04-11 23:34:32 -0400

    Print a list of highs.

--- data_analysis/sitka_highs.py
+++ data_analysis/sitka_highs.py
@@ -1,9 +1,18 @@
 import csv
 
-filename = 'data/sitka_weather_2018_simple.csv'
+filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
+    # Print the header row
+    # print(header_row)
+    # print the header row to find row of interest.
+    # for index, column_header in enumerate(header_row):
+    #    print(f"{index}, {column_header}")
+    # Get high temperatures, column 5, from this file.
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
 
-    for index, column_header in enumerate(header_row):
-        print(f"{index}, {column_header}")
+print(highs)
