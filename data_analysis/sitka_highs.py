bea3ded5ac472553883857358b9a056546f7d974
JAL-code <jleffek@live.com>
2021-04-11 23:48:43 -0400

    Print a simple plot of highs for SITKA AIRPORT.

--- data_analysis/sitka_highs.py
+++ data_analysis/sitka_highs.py
@@ -1,5 +1,7 @@
 import csv
 
+import matplotlib.pyplot as plt
+
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -15,4 +17,23 @@ with open(filename) as f:
         high = int(row[5])
         highs.append(high)
 
-print(highs)
+# print(highs) # print to output.
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots() #figsize=(10, 6), dpi=128
+# smaller size for more values and set point color
+ax.plot(highs, c='red') 
+
+# Set chart title and label axis.
+ax.set_title("Daily high temperatures, July 2018", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+
+# Set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+# Set the range for each axis.
+# ax.axis([0, 1100, 0, 1100000])
+
+# plt.savefig('squares_plot_green.png', bbox_inches='tight')
+plt.show()
