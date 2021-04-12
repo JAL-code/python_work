c145a59e8b698a4454aa5a4ff170f7893915626f
JAL-code <jleffek@live.com>
2021-04-11 20:06:42 -0400

    Plot with one point and point size.

--- data_analysis/scatter_squares.py
+++ data_analysis/scatter_squares.py
@@ -1,7 +1,18 @@
 import matplotlib.pyplot as plt
 
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4)
+ax.scatter(2, 4, s=200)
+
+# Set chart title and label axis.
+ax.set_title("Square Numbers", fontsize=14)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of value", fontsize=14)
+
+# Set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize=14)
 
 plt.show()
